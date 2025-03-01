07721c437868a63e817df367a4e92cebd2ff524f
TanmoyDhar1077 <tanmoydhar1077@gmail.com>
2025-03-02 00:01:53 +0600

    Clock Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,37 @@
+// Background color change
 let changeTheme = document.getElementById('themeBtn');
 
 function randomBackground() {
     let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
     return randomColor;
 }
-changeTheme.addEventListener('click', function(event) {
+changeTheme.addEventListener('click', function (event) {
     event.preventDefault();
     document.body.style.backgroundColor = randomBackground();
 });
+
+// Date and Week Update
+function updateDateWeek() {
+    const now = new Date();
+
+    // Get the day of the week
+    const day = now.toLocaleDateString('en-US', { weekday: 'short' });
+
+    // Get the full date
+    const fullDate = now.toLocaleDateString('en-US', {
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric'
+    });
+
+    document.getElementById('updateDate').innerHTML =
+        `<span class="text-[22px]">${day} ,</span><br>
+         <span class="text-[22px] font-bold">${fullDate}</span>`;
+    // console.log(day);
+    // console.log(fullDate);
+};
+
+updateDateWeek();
+
+// Click Button to show Active log
+
